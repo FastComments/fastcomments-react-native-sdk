d2396797e39db523c21310310d19112069be35e1
winrid <winrid@gmail.com>
2022-08-24 18:54:54 -0700

    child comments and related styling

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -5,7 +5,7 @@ import {useState} from 'react';
 import RenderHtml from 'react-native-render-html';
 
 import {FastCommentsState} from "../types/fastcomments-state";
-import {Image, StyleSheet, Text, useWindowDimensions, View} from "react-native";
+import {Image, Pressable, StyleSheet, Text, useWindowDimensions, View} from "react-native";
 import {CommentMenu} from "./comment-menu";
 import {CommentNotices} from "./comment-notices";
 import {CommentUserInfo} from "./comment-user-info";
@@ -18,6 +18,25 @@ import {FastCommentsImageAsset} from "../types/image-asset";
 export interface FastCommentsCommentWithState {
     comment: State<FastCommentsWidgetComment>;
     state: State<FastCommentsState>;
+}
+
+function setReplyBoxOpen(state: State<FastCommentsState>, commentId: string, isOpen: boolean) {
+    if (!state.commentState[commentId]?.get()) {
+        state.commentState.set((commentState) => {
+            if (!commentState[commentId]) {
+                commentState[commentId] = {
+                    replyBoxOpen: isOpen
+                }
+            } else {
+                commentState[commentId].replyBoxOpen = isOpen;
+            }
+            return {...commentState};
+        })
+    } else {
+        state.commentState[commentId].merge({
+            replyBoxOpen: isOpen
+        });
+    }
 }
 
 export function FastCommentsCommentView(commentWithState: FastCommentsCommentWithState) {
@@ -37,15 +56,17 @@ export function FastCommentsCommentView(commentWithState: FastCommentsCommentWit
 
     // Technically having a separate timer per comment is not optimal. But, JS timers are very light and we'll only render 30 comments most of the time.
     // It would be cool to have only one timer, like in the VanillaJS library.
-    setInterval(function() {
+    setInterval(function () {
         setDisplayDate(getDisplayDate(state.config.get(), state.translations.get(), dateObj));
     }, 60_000);
 
-    const { width } = useWindowDimensions();
+    const {width} = useWindowDimensions();
+    const isReplyBoxOpen = state.commentState[comment._id.get()]?.replyBoxOpen?.get();
 
+    // @ts-ignore
     return <View>
         <View style={styles.topRight}>
-            {comment.isPinned.get() && <Image source={state.imageAssets.get()[FastCommentsImageAsset.ICON_PIN_RED]} style={{width: 24, height: 24}} />}
+            {comment.isPinned.get() && <Image source={state.imageAssets[FastCommentsImageAsset.ICON_PIN_RED].get()} style={{width: 24, height: 24}}/>}
             {!(state.config.readonly.get()) && CommentMenu(commentWithState)}
         </View>
         <CommentNotices comment={comment} state={state}/>
@@ -57,11 +78,20 @@ export function FastCommentsCommentView(commentWithState: FastCommentsCommentWit
                 <View style={styles.commentBottomToolbarVote}>
 
                 </View>
-                <View style={styles.commentBottomToolbarReply}>
-
-                </View>
+                <Pressable style={styles.commentBottomToolbarReply} onPress={() => setReplyBoxOpen(state, comment._id.get(), !isReplyBoxOpen)}>
+                    <Image
+                        source={state.imageAssets[isReplyBoxOpen ? FastCommentsImageAsset.ICON_REPLY_ARROW_ACTIVE : FastCommentsImageAsset.ICON_REPLY_ARROW_INACTIVE].get()}
+                        style={{width: 15, height: 15}}/>
+                    <Text style={styles.commentBottomToolbarReplyText}>{state.translations.REPLY.get()}</Text>
+                </Pressable>
             </View>
-            {state.commentState[comment._id.get()]?.replyBoxOpen && <ReplyArea state={state} parentComment={comment} />}
+            {isReplyBoxOpen && <ReplyArea state={state} parentComment={comment}/>}
+        </View>
+        <View style={styles.children}>
+            {/* TODO how to fix stupid cast here? */}
+            {comment.children?.get() && (comment.children as State<FastCommentsWidgetComment[]>).map((comment) =>
+                <FastCommentsCommentView comment={comment} state={state} key={comment._id.get()}/>
+            )}
         </View>
     </View>;
 }
@@ -70,12 +100,28 @@ const styles = StyleSheet.create({
     topRight: {
         position: "absolute",
         top: 0,
-        right: 0
+        right: 0,
+        zIndex: 1
     },
     pin: {},
     userInfo: {},
     commentBottom: {},
-    commentBottomToolbar: {},
+    commentBottomToolbar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
     commentBottomToolbarVote: {},
-    commentBottomToolbarReply: {},
+    commentBottomToolbarReply: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    commentBottomToolbarReplyText: {
+        marginLeft: 3
+    },
+    children: {
+        "marginTop": 15,
+        "marginRight": 0,
+        "marginBottom": 0,
+        "marginLeft": 15
+    },
 })
