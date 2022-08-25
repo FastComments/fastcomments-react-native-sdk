7409e1de6a5e85c50c9b91c22e755a347e570490
winrid <winrid@gmail.com>
2022-08-24 18:53:42 -0700

    remove scrollview from CommentsList (moving responsibility to app)

--- src/components/comments-list.tsx
+++ src/components/comments-list.tsx
@@ -3,14 +3,12 @@ import * as React from 'react';
 
 import {FastCommentsState} from "../types/fastcomments-state";
 import {FastCommentsCommentView} from "./comment";
-import {ScrollView} from "react-native";
 import {State} from "@hookstate/core";
 
 export function CommentsList(state: State<FastCommentsState>) {
-    console.log('comments list', state.commentsTree.length);
-    return <ScrollView>
-        {state.commentsTree.map((comment) =>
-            <FastCommentsCommentView comment={comment} state={state} key={comment._id.get()}/>
-        )}
-    </ScrollView>
+    console.log('comments list', state.commentsTree.length); // TODO remove
+    // TODO have configuration option to wrap in ScrollView
+    return state.commentsTree.map((comment) =>
+        <FastCommentsCommentView comment={comment} state={state} key={comment._id.get()}/>
+    )
 }
