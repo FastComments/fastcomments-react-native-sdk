1cd6c8a7415fce66bccea342603077c4436a5981
winrid <winrid@gmail.com>
2022-08-30 18:48:26 -0700

    improved sort direction selector

--- src/components/select-sort-direction.tsx
+++ src/components/select-sort-direction.tsx
@@ -2,28 +2,48 @@
 import * as React from 'react';
 
 import {FastCommentsSortDirection, FastCommentsState} from "../types/fastcomments-state";
-import DropDownPicker from 'react-native-dropdown-picker';
-import {useState} from "react";
 import {State} from "@hookstate/core";
+import {Image, StyleSheet, View, Text} from 'react-native';
+import {FastCommentsImageAsset} from "../types/image-asset";
+import {ModalMenu} from "./modal-menu";
+
+const SortDirectionTranslationsById: Record<string, string> = {
+    'OF': 'OLDEST_FIRST',
+    'NF': 'NEWEST_FIRST',
+    'MR': 'MOST_RELEVANT',
+};
 
 export function SelectSortDirection(state: State<FastCommentsState>) {
-  const [open, setOpen] = useState(false);
-  const [items, setItems] = useState([
-    {label: state.translations.OLDEST_FIRST.get(), value: 'OF'},
-    {label: state.translations.NEWEST_FIRST.get(), value: 'NF'},
-    {label: state.translations.MOST_RELEVANT.get(), value: 'MR'},
-  ]);
+    const setValue = (newValue: any) => state.sortDirection.set(newValue as FastCommentsSortDirection);
+    const menuItems = [
+        {label: state.translations.OLDEST_FIRST.get(), id: 'OF', handler: () => { setValue('OF') } },
+        {label: state.translations.NEWEST_FIRST.get(), id: 'NF', handler: () => { setValue('NF') } },
+        {label: state.translations.MOST_RELEVANT.get(), id: 'MR', handler: () => { setValue('MR') } },
+    ];
 
-  const setValue = (newValue: any) => state.sortDirection.set(newValue as FastCommentsSortDirection);
+    // TODO dedicated down carrot icon
+    const openButton = <View style={styles.openButton}>
+        <Text style={styles.text}>{state.translations[SortDirectionTranslationsById[state.sortDirection.get()]].get()}</Text>
+        <Image source={state.imageAssets[FastCommentsImageAsset.ICON_DOWN_ACTIVE].get()} style={styles.downCarrot}/>
+    </View>;
 
-  return (
-    <DropDownPicker
-      open={open}
-      value={state.sortDirection.get()}
-      items={items}
-      setOpen={setOpen}
-      setValue={setValue}
-      setItems={setItems}
-    />
-  );
+    return <ModalMenu state={state} items={menuItems} openButton={openButton}/>;
 }
+
+const styles = StyleSheet.create({
+    openButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 5,
+    },
+    text: {
+        marginRight: 5,
+        fontSize: 12,
+        fontWeight: '500'
+    },
+    downCarrot: {
+        width: 14,
+        aspectRatio: 1,
+        resizeMode: 'contain'
+    }
+})
