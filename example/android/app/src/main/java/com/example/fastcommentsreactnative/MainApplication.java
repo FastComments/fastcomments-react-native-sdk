48efb6b6ab7e5addb23ae26c7e3a9a4d0c659790
winrid <winrid@gmail.com>
2022-09-02 17:35:55 -0700

    ripping out references to webview (unused)

--- example/android/app/src/main/java/com/example/fastcommentsreactnative/MainApplication.java
+++ example/android/app/src/main/java/com/example/fastcommentsreactnative/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,8 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for FastcommentsReactNativeExample:
-          packages.add(new RNCWebViewPackage());
 
           return packages;
         }
