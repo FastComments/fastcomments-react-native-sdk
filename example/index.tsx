bdec3bc92a5d7b3c320398f85ba931188dd0b2f2
winrid <winrid@gmail.com>
2022-09-07 17:15:45 -0700

    improved examples

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
-import App from './src/App';
-// import DarkModeApp from './src/DarkModeApp';
+import AppCommenting from './src/AppCommenting';
+import AppSecureSSODarkChat from './src/AppSecureSSODarkChat';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppCommenting);
--- example/src/AppCommenting.tsx
+++ example/src/AppCommenting.tsx
@@ -1,15 +1,22 @@
+// @ts-ignore - TODO REMOVE
 import * as React from 'react';
 
 import {FastCommentsLiveCommenting} from '../../index';
 import {useState} from "react";
+import { FastCommentsCommentWidgetConfig } from 'fastcomments-typescript';
+import {ScrollView} from "react-native";
 
-export default function App() {
+/**
+ * @description More traditional commenting-style widget, with input area at top and comments at bottom.
+ */
+export default function AppCommenting() {
   const myTenantId = 'demo'; // Your tenant id. Can be fetched from https://fastcomments.com/auth/my-account/api-secret
   const myAppPageId = 'native-test'; // the ID or URL of the comment thread in your app.
 
-  const [config] = useState({
+  const [config] = useState<FastCommentsCommentWidgetConfig>({
     tenantId: myTenantId,
-    urlId: myAppPageId
+    urlId: myAppPageId,
+    showLiveRightAway: true
   });
 
   // Uncomment this to test changing pages without reloading the whole widget.
@@ -24,6 +31,6 @@ export default function App() {
   // }, []);
 
   return (
-    <FastCommentsLiveCommenting config={config}/>
+      <ScrollView style={{margin: 5}}><FastCommentsLiveCommenting config={config}/></ScrollView>
   );
 }
