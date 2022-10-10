9881adc04264768644e8992bc37f2ba3226f30d6
winrid <winrid@gmail.com>
2022-10-10 14:54:21 -0700

    Fixing comment pinning and various exceptions around pinning

--- src/services/comment-positioning.ts
+++ src/services/comment-positioning.ts
@@ -3,10 +3,13 @@ import {FastCommentsCommentPositions} from "../types/dto/websocket-live-event";
 import {State} from "@hookstate/core";
 
 export function repositionComment(id: string, commentPositions: FastCommentsCommentPositions, state: State<FastCommentsState>) {
+    // console.log('BEGIN repositionComment', id, JSON.stringify(commentPositions));
+
+    // we delete the comment from the tree, and then add it back into the correct spot.
     state.commentsTree.set((commentsTree) => {
         let currentIndex: number;
         for (let i = 0; i < state.commentsTree.length; i++) {
-            if (state.commentsTree[i]._id.get() === id) {
+            if (state.commentsTree[i]._id.get({stealth: true}) === id) {
                 currentIndex = i;
                 break;
             }
@@ -28,13 +31,16 @@ export function repositionComment(id: string, commentPositions: FastCommentsComm
                 break;
             }
             if (state.commentsTree[i]._id.get() === after) {
-                newIndex = i - 1;
+                newIndex = Math.max(i - 1, 0);
                 break;
             }
         }
-        if (newIndex! > -1) {
-            commentsTree.splice(newIndex!, 0, state.commentsById[id].get());
-        }
+        // checking for > -1 here breaks pin and unpin because if it's -1 we want to add back to the tree, for example when there is only
+        // one page.
+        // noproxy is important here because otherwise we put a state object into the tree where only raw objects should be, inside a parent state object.
+        // this will cause things to blow up on re-render and trying to modify the comment object (like pinning again), since we have no run-time
+        // type checking. :)
+        commentsTree.splice(newIndex!, 0, state.commentsById[id].get({stealth: true, noproxy: true}));
         return commentsTree;
     });
 }
