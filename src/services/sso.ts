84f8d2e3cc5b7be64e97a19d22f108cae6fdf1bd
winrid <winrid@gmail.com>
2025-09-05 16:06:59 -0700

    Fix build issues

--- src/services/sso.ts
+++ src/services/sso.ts
@@ -2,7 +2,7 @@ import { FastCommentsCommentWidgetConfig } from "fastcomments-typescript";
 
 // this should be safe to call multiple times for the same config object
 export function mergeSimpleSSO(config: FastCommentsCommentWidgetConfig) {
-    if (config.simpleSSO !== undefined) {
+    if (config.simpleSSO !== undefined && config.simpleSSO !== null) {
         // have to check email since gets mapped to id
         if (config.simpleSSO.email) {
             config.sso = {
